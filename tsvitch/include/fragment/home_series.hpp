cf6480b2698348cab2ce8ab9f0c5ce7ce4b44e1d
YusufGuntekin95 <guntekinyusuf1903@gmail.com>
2026-09-04 18:30:08 +0300

    Add NX Media series view

---
+++ tsvitch/include/fragment/home_series.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <borealis/core/bind.hpp>
+#include <borealis/core/box.hpp>
+#include "view/recycling_grid.hpp"
+#include "utils/xtream_helper.hpp"
+
+class HomeSeries : public brls::Box {
+public:
+    HomeSeries();
+    ~HomeSeries() override = default;
+
+    void reload();
+    void openSeries(const tsvitch::XtreamSeries& series);
+    void downloadFocused();
+
+    static brls::View* create();
+
+private:
+    void showEpisodePicker(const tsvitch::XtreamSeries& series,
+                           const tsvitch::XtreamSeriesInfo& info,
+                           bool downloadMode);
+
+    BRLS_BIND(RecyclingGrid, recyclingGrid, "nx/series/grid");
+    BRLS_BIND(brls::Label, statusLabel, "nx/series/status");
+};
